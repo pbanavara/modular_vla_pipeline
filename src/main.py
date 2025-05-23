515ccb00e7d8211ccc7d12b53eb29cf47e0b88f1
Pradeep Banavara <pradeepbs@gmail.com>
2025-05-23 13:17:50 -0700

    Removed unwanted elements from MujocoExecutor Finished frst stage of async pipeline

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from pipeline import pipeline
+from pipeline.async_sim.async_simulation import MujocoRealtimeExecutor
+from utils.utilities import get_resolved_path
+import asyncio
 
 def main():
-    pipeline.run_pipeline()
+    #pipeline.run_pipeline()
+    mujoco_model_path = str(get_resolved_path("../simulated_sink/aloha/aloha.xml"))
+    executor = MujocoRealtimeExecutor(mujoco_model_path)
+    asyncio.run(executor.start())
 
 if __name__ == "__main__":
     main()
